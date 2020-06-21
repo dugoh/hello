ca43a171082ab6b108e79d6d398858eaaa2d06bf
Jacob Goense <dugo@xs4all.nl>
2020-06-21 13:26:11 +0200

    Update run.cmd

--- run.cmd
+++ run.cmd
@@ -3,4 +3,6 @@ set
 "c:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
 echo +++ after +++
 set
+cd simh
+cd "Visual Studio Projects"
 vcbuild /M%NUMBER_OF_PROCESSORS% /useenv /rebuild Simh.sln "Release|Win32"
