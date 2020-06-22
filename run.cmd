972dadbb0403cbf5830a6cedd3f719813f26bced
Jacob Goense <dugo@xs4all.nl>
2020-06-22 13:43:46 +0200

    Update run.cmd

--- run.cmd
+++ run.cmd
@@ -3,5 +3,11 @@ git clone https://github.com/simh/simh.git
 cd "simh\Visual Studio Projects"
 vcbuild /M%NUMBER_OF_PROCESSORS% /useenv /rebuild Simh.sln "Release|Win32"
 echo done1
+dir /s /b *.exe
 echo done2
+cd ..
+dir /s /b *.exe
 echo done3
+cd ..
+dir /s /b *.exe
+
