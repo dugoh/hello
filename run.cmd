f9d4dba32595a2860cd38581c2935ebf95accfde
Jacob Goense <dugo@xs4all.nl>
2020-06-23 00:00:10 +0200

    Update run.cmd

--- run.cmd
+++ run.cmd
@@ -1,12 +1,15 @@
 git clone https://github.com/simh/simh.git
 cd simh
 build_vstudio.bat BuildROMs VAX|more
+echo
 echo ++++++++++ done1
 type c:\projects\hello\simh\BIN\NT\Project\simh\VAX\Win32-Release\BuildLog.htm
+echo
 echo ++++++++++ done2
 type c:\projects\hello\simh\BIN\NT\Project\simh\BuildROMs\Win32-Release\BuildLog.htm
+echo
 echo ++++++++++ done3
 cd ..
 copy C:\projects\hello\simh\BIN\NT\Win32-Release\MicroVAX3900.exe .\
 echo sh ver| microvax3900
-bash.exe -c 'echo sh ver| microvax3900'
+bash.exe -c 'echo sh ver| ./microvax3900'
