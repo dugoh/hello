7e298999cd805ec2ae16c6aec4540456198e4907
Jacob Goense <dugo@xs4all.nl>
2020-06-22 20:43:28 +0200

    Update run.cmd

--- run.cmd
+++ run.cmd
@@ -3,7 +3,7 @@ git clone https://github.com/simh/simh.git
 REM cd "simh\Visual Studio Projects"
 REM vcbuild /M%NUMBER_OF_PROCESSORS% /useenv /rebuild Simh.sln "Release|Win32"
 cd simh
-build_vstudio.bat BuildROMs VAX|REM
+build_vstudio.bat BuildROMs VAX|more
 echo done1
 dir /s /b *.exe
 echo done2
