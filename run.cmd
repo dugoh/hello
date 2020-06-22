016de307e9d3a846e44c726f2a818040068d2ed2
Jacob Goense <dugo@xs4all.nl>
2020-06-22 22:05:56 +0200

    Update run.cmd

--- run.cmd
+++ run.cmd
@@ -1,23 +1,12 @@
-REM this is dos, right!?
-gcc -o hello.exe hello.c 
-hello.exe
 git clone https://github.com/simh/simh.git
-REM cd "simh\Visual Studio Projects"
-REM vcbuild /M%NUMBER_OF_PROCESSORS% /useenv /rebuild Simh.sln "Release|Win32"
 cd simh
 build_vstudio.bat BuildROMs VAX|more
-echo done1
-bash.exe -c 'echo m00; pwd; ls -l'
-dir /s /b *.exe
-echo done2
+echo ++++++++++ done1
+type c:\projects\hello\simh\BIN\NT\Project\simh\VAX\Win32-Release\BuildLog.htm
+echo ++++++++++ done2
+type c:\projects\hello\simh\BIN\NT\Project\simh\BuildROMs\Win32-Release\BuildLog.htm
+echo ++++++++++ done3
 cd ..
-dir /s /b *.exe
-echo done3
-cd ..
-dir /s /b *.exe
-ECHO done4
-PWD
-DIR C:\projects\hello\simh\BIN\NT\Win32-Release\
-bash.exe -c 'echo m00; pwd; ls -l'
-
-
+copy C:\projects\hello\simh\BIN\NT\Win32-Release\MicroVAX3900.exe .\
+echo sh ver| microvax3900
+bash.exe -c 'echo sh ver| microvax3900'
