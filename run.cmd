b7d153ec9a0dd0c9c4546739580754a13419bcb9
Jacob Goense <dugo@xs4all.nl>
2020-06-22 14:43:01 +0200

    Update run.cmd

--- run.cmd
+++ run.cmd
@@ -1,7 +1,9 @@
 REM this is dos, right!?
 git clone https://github.com/simh/simh.git
-cd "simh\Visual Studio Projects"
-vcbuild /M%NUMBER_OF_PROCESSORS% /useenv /rebuild Simh.sln "Release|Win32"
+#cd "simh\Visual Studio Projects"
+#vcbuild /M%NUMBER_OF_PROCESSORS% /useenv /rebuild Simh.sln "Release|Win32"
+cd simh
+build_vstudio.bat VAX
 echo done1
 dir /s /b *.exe
 echo done2
@@ -10,6 +12,3 @@ dir /s /b *.exe
 echo done3
 cd ..
 dir /s /b *.exe
-bash ls
-bash wget --help
-
